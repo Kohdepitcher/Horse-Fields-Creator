c7a5acfd6e22148f2df5c7a33736a6bba209ddaa
Kohde Pitcher <kohde1@hotmail.com>
2021-03-02 11:51:21 +1000

    added table and race selector + logic

--- fields-creator/src/app/app.component.ts
+++ fields-creator/src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//
+import { Race, RaceEntry, FieldsService } from "./fields.service";
+
+import { BehaviorSubject } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
+import { MatTableDataSource, MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'fields-creator';
+
+  races = new BehaviorSubject<Race[]>([]);
+
+  displayedColumns = ["number", "barrier", "name", "jockey", "weight"];
+
+  //table datasource
+  dataSource: MatTableDataSource<RaceEntry> | any
+
+  selectedRace: number = 0
+
+  constructor(private raceModel: FieldsService) {}
+
+  ngOnInit() {
+    this.raceModel.subscribe(this.races);
+
+    // this.raceModel.addRaceEntry(17,1,1,"test","test", 34)
+
+  }
+
+  //called when the race selector selects a race
+  loadRaceEntries(event: MatSelectChange) {
+
+    console.log(event.value);
+
+    //get the race from the model
+    const race = this.raceModel.getRace(event.value)
+
+    console.log(race.entries)
+
+    //set the datasource for the table to the race's entries
+    this.dataSource = new MatTableDataSource(race.entries)
+    
+
+  }
 }
+
+
