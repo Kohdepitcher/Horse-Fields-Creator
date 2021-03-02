d6ebdcfac952dd24257d82f65b64d71c01607418
Kohde Pitcher <kohde1@hotmail.com>
2021-03-02 11:51:04 +1000

    added imports

--- fields-creator/src/app/app.module.ts
+++ fields-creator/src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//material imports
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from'@angular/material/icon';
+import {MatButtonModule} from "@angular/material/button";
+import {MatChipsModule} from '@angular/material/chips';
+import { MatTableModule } from "@angular/material/table";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatTableModule,
+    MatSelectModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
