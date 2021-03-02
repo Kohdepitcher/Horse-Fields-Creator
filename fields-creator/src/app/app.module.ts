241b8da247010c811428b847c8022da23f4561f4
Kohde Pitcher <kohde1@hotmail.com>
2021-03-02 20:10:18 +1000

    added more imports

--- fields-creator/src/app/app.module.ts
+++ fields-creator/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 //material imports
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from'@angular/material/icon';
@@ -12,21 +14,35 @@ import {MatChipsModule} from '@angular/material/chips';
 import { MatTableModule } from "@angular/material/table";
 import { MatSelectModule } from "@angular/material/select";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatMenuModule } from "@angular/material/menu";
+import { RaceDialogComponent } from './race-dialog/race-dialog.component';
+import { MatDialogModule } from "@angular/material/dialog";
+import {MatInputModule} from '@angular/material/input';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RaceDialogComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatChipsModule,
     MatTableModule,
     MatSelectModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
