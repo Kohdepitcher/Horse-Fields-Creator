d2ed9700e0560e35d8f17e8972511d3329c54b07
Kohde Pitcher <kohde1@hotmail.com>
2021-03-02 18:15:48 +1000

    created race dialog

---
+++ fields-creator/src/app/race-dialog/race-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RaceDialogComponent } from './race-dialog.component';
+
+describe('RaceDialogComponent', () => {
+  let component: RaceDialogComponent;
+  let fixture: ComponentFixture<RaceDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RaceDialogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RaceDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ fields-creator/src/app/race-dialog/race-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-race-dialog',
+  template: `
+    <p>
+      race-dialog works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class RaceDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
